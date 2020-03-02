8b773e15ae7d6e3455e490acc682808bf5078bf6
Mukesh Pednekar <Mukesh_Pednekar@yahoo.com>
2020-03-02 22:14:00 +0530

    Typo fixed in the method name. Changed from subtractTowWholeNumbers -> subtractTwoWholeNumbers

--- src/test/java/CalculatorSubtractionTest.java
+++ src/test/java/CalculatorSubtractionTest.java
@@ -5,12 +5,13 @@ import org.junit.Test;
 public class CalculatorSubtractionTest {
 
     @Test
-    public void subtractTowWholeNumbers() {
+    public void subtractTwoWholeNumbers() {
         BasicCalculator baseCalculator = new BasicCalculator();
         int a =10;
         int b=20;
         int result = baseCalculator.subtractNumbers(a, b);
         Assert.assertEquals(-10, result);
     }
+
 
 }
