a8fd1508e719cb62c99b34ae6f96cf33e8771089
Mukesh Pednekar <Mukesh_Pednekar@yahoo.com>
2020-03-05 21:32:35 +0530

    invalid test to check jenkins auto run

--- src/test/java/CalculatorSubtractionTest.java
+++ src/test/java/CalculatorSubtractionTest.java
@@ -8,7 +8,7 @@ public class CalculatorSubtractionTest {
     public void subtractTwoWholeNumbers() {
         BasicCalculator baseCalculator = new BasicCalculator();
         int a =10;
-        int b=30;
+        int b=50;
         int result = baseCalculator.subtractNumbers(a, b);
         Assert.assertEquals(-10, result);
     }
